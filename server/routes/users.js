f6e54f60258f237b8dae0592962b6edb703e93ef
tamar-nisan <tamarnisan@hilms.tech>
2024-11-27 14:59:51 +0200

    delete user by id

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,13 +8,30 @@ router.get("/", function (req, res, next) {
         res.send(result);
     });
 });
-
+/*Get todos by user_id*/
 router.get("/:id/todos", function (req, res, next) {
     con.query(`SELECT * FROM to_do WHERE user_id=${req.params.id}`, (err, result) => {
-        if (err) throw err;
-        res.status(200).send(result);
+        if (err) res.status(400).send("Something went wrong.");
+        else { res.status(200).send(result); }
     });
-    res.status(400).send("Something went wrong.");
+
 });
+/*Get info by user_id*/
+router.get("/:id", function (req, res, next) {
+    con.query(`SELECT * FROM user WHERE id=${req.params.id}`, (err, result) => {
+        if (err) res.status(400).send("Something went wrong.");
+        else { res.status(200).send(result); }
+    });
+
+});
+/*Delete info by user_id*/
+router.delete("/:id", function (req, res, next) {
+    con.query(`DELETE  FROM user WHERE id=${req.params.id}`, (err, result) => {
+        if (err) res.status(400).send("Something went wrong.");
+        else { res.status(200).send("user deleted"); }
+    });
+
+});
+
 
 module.exports = router;
