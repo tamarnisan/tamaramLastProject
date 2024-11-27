3dab044dd67157b43b97a94e82fe15929fb5e8fb
tamar-nisan <tamarnisan@hilms.tech>
2024-11-27 13:19:17 +0200

    add login

--- server/routes/logIn.js
+++ server/routes/logIn.js
@@ -5,11 +5,12 @@ const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "z10mz10m",
-    database: "deme",
+    database: "social_db",
 });
 router.post("/", (req, res) => {
     /////לזכור להביא את הסיסמא מטבלה נפרדת
-    const sql = `SELECT * FROM user WHERE username='${req.body.username}' AND password='${req.body.password}'`;
+    const sql = `SELECT * FROM password WHERE username='${req.body.username}' AND password='${req.body.password}'`;
+    console.log('sql: ', sql);
     con.query(sql, function (err, result) {
         if (err) return res.status(400).send("something went wrong");
         console.log("result.length: ", result.length);
@@ -17,7 +18,7 @@ router.post("/", (req, res) => {
             return res.status(400).send("please register");
         } else {
             // כשיהיה עוד טבךא פשוט להביא הכל *
-            const sql = `SELECT id, username, email, phone, name FROM user WHERE username='${req.body.username}'`;
+            const sql = `SELECT * FROM user WHERE username='${req.body.username}'`;
             con.query(sql, function (err, result) {
                 if (err) return res.status(400).send("something went wrong");
                 else {
--- server/routes/register.js
+++ server/routes/register.js
@@ -5,23 +5,46 @@ const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "z10mz10m",
-    database: "deme",
+    database: "social_db",
 });
 router.post("/", (req, res) => {
+    console.log('req.body.username: ', req.body.username);
     const sql = `SELECT * FROM user WHERE username='${req.body.username}'`;
-    con.query(sql, function (err, result) {
-        if (err) return res.status(400).send("something went wrong");
-        if (result.length === 0) {
-            const sql = `INSERT INTO user (username, password, email, phone, name ) VALUES ('${req.body.username}','${req.body.password}', '${req.body.email}', '${req.body.phone}', '${req.body.name}')`;
-            con.query(sql, function (err, result) {
-                if (err) return res.status(400).send("something went wrong");
-                console.log("user inserted");
-                return res.status(200).send(`welcome ${req.body.username}`);
-            });
-        } else {
-            return res.status(400).send("this username is already exist");
-        }
-    });
+    try {
+        con.query(sql, function (err, result) {
+
+            console.log('result.length: ', result);
+
+            if (result.length === 0) {
+                console.log("hiii")
+                const sql = `INSERT INTO user (username, email, phone, name ) VALUES ('${req.body.username}', '${req.body.email}', '${req.body.phone}', '${req.body.name}')`;
+                con.query(sql, function (err, result) {
+                    if (err) return res.status(400).send("insert to users");
+                    console.log("user inserted");
+                    console.log("before sql2");
+                    const sql2 = `INSERT INTO password (username, password) VALUES ('${req.body.username}','${req.body.password}')`;
+                    con.query(sql2, function (err, result) {
+                        if (err) return res.status(400).send("insert yo password");
+                        console.log("user password inserted");
+
+                        const sql3 = `SELECT * FROM user WHERE username='${req.body.username}'`;
+                        con.query(sql3, function (err, result) {
+                            if (err) return res.status(400).send("select all user");
+                            else {
+                                console.log(result[0]);
+                                return res.status(200).send(result[0]);
+                            }
+                        });
+                    });
+                });
+            }
+            else {
+                return res.status(400).send("this username is already exist");
+            }
+        })
+    } catch (err) {
+        return res.status(400).send("something went wrong");
+    }
 });
 
 module.exports = router;
