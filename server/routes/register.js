a18310670db1c47a99aeb93d62e4f6df80fca08a
tamar-nisan <tamarnisan@hilms.tech>
2024-11-27 12:02:02 +0200

    log in register

---
+++ server/routes/register.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "z10mz10m",
+    database: "deme"
+
+
+});
+router.post('/', (req, res) => {
+    const sql = `SELECT * FROM user WHERE username='${req.body.username}'`;
+    con.query(sql, function (err, result) {
+        if (err) return res.status(400).send("something went wrong");
+        if (result.length === 0) {
+            var sql = `INSERT INTO user (username, password, email, phone, name ) VALUES ('${req.body.username}','${req.body.password}', '${req.body.email}', '${req.body.phone}', '${req.body.name}')`;
+            con.query(sql, function (err, result) {
+                if (err) return res.status(400).send("something went wrong");
+                console.log("user inserted");
+                return res.status(200).send(`welcome ${req.body.username}`)
+            });
+        }
+        else {
+            return res.status(400).send("this username is already exist");
+        }
+    });
+
+
+});
+
+module.exports = router;
+
