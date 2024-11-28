af934062c30004b9266a8f1c093fb3660c11141c
tamar-nisan <tamarnisan@hilms.tech>
2024-11-28 14:25:59 +0200

    add

--- client/src/components/ProfileToDos.jsx
+++ client/src/components/ProfileToDos.jsx
@@ -1,5 +1,27 @@
+
+
+import { useState, useEffect } from "react";
+
+import { useNavigate, useParams } from "react-router-dom";
+import GetRequest from "../utilities/getRequest";
+
 function ProfileToDos() {
+    const { id } = useParams();
+    const [todosarray, setTodosarray] = useState([]);
+    const [error, setError] = useState(null);
+    const getTodos = async (e) => {
+        const url = `http://localhost:3000/users/${id}/todos`;
+        const response = await GetRequest(url);
+        if (response.status === 200) {
+            setTodosarray(response.res);
+        }
+        console.log(todosarray)
+    };
+    
+    useEffect(() => {
+        getTodos();
+    }, []);
+    
     return "I am Profile ToDos";
 }
-
 export default ProfileToDos;
