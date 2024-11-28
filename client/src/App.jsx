931e9f06c7e17b5fa9ac3889377dc36b50e3a50e
tamar-nisan <tamarnisan@hilms.tech>
2024-11-28 09:49:41 +0200

    register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import LogIn from './LogIn'
+import Register from './Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 <>
-<LogIn/>
+{/* <LogIn/> */}
+<Register/>
 </>
   )
 }
